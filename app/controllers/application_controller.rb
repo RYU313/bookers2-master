020e944acbdda243c2a85f9508d79adf39dcf8fe
RYU313 <umaryu313@gmail.com>
2021-09-23 16:19:13 +0000

    Homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_pemitted_paramenters,if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    user_path(@user)
+  end
   protected
   def configure_pemitted_paramenters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
